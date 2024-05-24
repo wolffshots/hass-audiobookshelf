e49e32a29dac347de4e47838a25b82d771705652
jadon <16850875+wolffshots@users.noreply.github.com>
2024-05-24 17:30:43 +0200

    chore!: rewrite it all

--- custom_components/audiobookshelf/__init__.py
+++ custom_components/audiobookshelf/__init__.py
@@ -1,172 +1,32 @@
-"""Init for audiobookshelf integration"""
-
-import asyncio
+"""Custom component for Audiobookshelf."""
 import logging
+from homeassistant.helpers import discovery
+# from .sensor import async_refresh_libraries
 
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import Config, HomeAssistant
-from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
-from requests import HTTPError, Timeout
+DOMAIN = "audiobookshelf"
 
-from custom_components.audiobookshelf.api import AudiobookshelfApiClient
+_LOGGER = logging.getLogger(__name__)
 
-from .const import (
-    CONF_ACCESS_TOKEN,
-    CONF_HOST,
-    DOMAIN,
-    ISSUE_URL,
-    PLATFORMS,
-    SCAN_INTERVAL,
-    VERSION,
-)
+# async def async_setup(hass, config):
+#     """Set up the Audiobookshelf component."""
+#     # Schedule the setup of sensor platform
+#     hass.async_create_task(discovery.async_load_platform(hass, "sensor", DOMAIN, {}, config))
+#     hass.async_create_task(async_refresh_libraries(hass))
 
-_LOGGER: logging.Logger = logging.getLogger(__package__)
+#     return True
 
+async def async_setup(hass, config):
+    """Set up the Audiobookshelf component."""
+    # Schedule the setup of sensor platform
+    hass.async_create_task(discovery.async_load_platform(hass, "sensor", DOMAIN, {}, config))
 
-class AudiobookshelfDataUpdateCoordinator(DataUpdateCoordinator):
-    """Class to manage fetching data from the API."""
+    # Use a helper to get the async_add_entities function from the sensor platform setup
+    # async def platform_setup():
+    #     """Wait for platform to be set up and then start refreshing libraries."""
+    #     platform = hass.data.get('sensor_platform')
+    #     if platform:
+    #         await async_refresh_libraries(hass, platform.async_add_entities)
 
-    def __init__(
-        self,
-        hass: HomeAssistant,
-        client: AudiobookshelfApiClient,
-    ) -> None:
-        """Initialize."""
-        self.api = client
-        self.platforms = []
+    # hass.async_create_task(platform_setup())
 
-        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=SCAN_INTERVAL)
-
-    async def _async_update_data(self) -> dict[str, None]:
-        """Update data via library."""
-        update = {"connectivity": None, "users": None, "sessions": None}
-        try:
-            connectivity_update = await self.api.api_wrapper(
-                method="get",
-                url=self.api.get_host() + "/ping",
-            )
-            _LOGGER.debug(
-                """async_update connectivity_update: %s""",
-                connectivity_update,
-            )
-            update["connectivity"] = connectivity_update
-        except ConnectionError:
-            update["connectivity"] = "ConnectionError: Unable to connect."
-        except (TimeoutError, Timeout):
-            update["connectivity"] = "TimeoutError: Request timed out."
-        except HTTPError as http_error:
-            update["connectivity"] = f"HTTPError: Generic HTTP Error happened {http_error}"
-        try:
-            users_update = await self.api.api_wrapper(
-                method="get",
-                url=self.api.get_host() + "/api/users",
-            )
-            num_users = self.api.count_active_users(users_update)
-            _LOGGER.debug("""async_update num_users: %s""", num_users)
-            update["users"] = num_users
-        except ConnectionError:
-            update["users"] = "ConnectionError: Unable to connect."
-        except (TimeoutError, Timeout):
-            update["users"] = "TimeoutError: Request timed out."
-        except HTTPError as http_error:
-            update["users"] = f"HTTPError: Generic HTTP Error happened {http_error}"
-        try:
-            online_users_update = await self.api.api_wrapper(
-                method="get",
-                url=self.api.get_host() + "/api/users/online",
-            )
-            open_sessions = self.api.count_open_sessions(online_users_update)
-            _LOGGER.debug("""async_update open_sessions: %s""", open_sessions)
-            update["sessions"] = open_sessions
-        except ConnectionError:
-            update["sessions"] = "ConnectionError: Unable to connect."
-        except (TimeoutError, Timeout):
-            update["sessions"] = "TimeoutError: Request timed out."
-        except HTTPError as http_error:
-            update["sessions"] = f"HTTPError: Generic HTTP Error happened {http_error}"
-        try:
-            library_stats_update = await self.api.api_wrapper(
-                method="get",
-                url=self.api.get_host() + "/api/libraries",
-            )
-            update["libraries"] = library_stats_update
-        except ConnectionError:
-            update["libraries"] = "ConnectionError: Unable to connect."
-        except (TimeoutError, Timeout):
-            update["libraries"] = "TimeoutError: Request timed out."
-        except HTTPError as http_error:
-            update["libraries"] = f"HTTPError: Generic HTTP Error happened {http_error}"
-        return update
-
-async def async_setup(hass: HomeAssistant, config: Config) -> bool:
-    """Setting up this integration using YAML is not supported."""
     return True
-
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
-) -> bool:
-    """Set up this integration using UI."""
-    if hass.data.get(DOMAIN) is None:
-        hass.data.setdefault(DOMAIN, {})
-        _LOGGER.info(
-            """
-            -------------------------------------------------------------------
-            Audiobookshelf
-            Version: %s
-            This is a custom integration!
-            If you have any issues with this you need to open an issue here:
-            %s
-            -------------------------------------------------------------------
-            """,
-            VERSION,
-            ISSUE_URL,
-        )
-
-    host = entry.data.get(CONF_HOST)
-    access_token = entry.data.get(CONF_ACCESS_TOKEN)
-
-    session = async_get_clientsession(hass)
-    client = AudiobookshelfApiClient(host, access_token, session)
-
-    coordinator = AudiobookshelfDataUpdateCoordinator(hass=hass, client=client)
-    await coordinator.async_refresh()
-
-    if not coordinator.last_update_success:
-        raise ConfigEntryNotReady
-
-    hass.data[DOMAIN][entry.entry_id] = coordinator
-
-    for platform in PLATFORMS:
-        if entry.options.get(platform, True):
-            coordinator.platforms.append(platform)
-            hass.async_create_task(
-                hass.config_entries.async_forward_entry_setup(entry, platform),
-            )
-    entry.add_update_listener(async_reload_entry)
-    return True
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Handle removal of an entry."""
-    coordinator = hass.data[DOMAIN][entry.entry_id]
-    unloaded = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, platform)
-                for platform in PLATFORMS
-                if platform in coordinator.platforms
-            ],
-        ),
-    )
-    if unloaded:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unloaded
-
-async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Reload config entry."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
--- custom_components/audiobookshelf/sensor.py
+++ custom_components/audiobookshelf/sensor.py
@@ -1,91 +1,291 @@
-"""Sensor platform for Audiobookshelf."""
+
+import asyncio
 import logging
+from typing import Any
+import aiohttp
+from datetime import timedelta
+from homeassistant.helpers.entity_registry import async_get as async_get_entity_registry, EntityRegistry
 
-from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.update_coordinator import (
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import DOMAIN
-from .entity import AudiobookshelfEntity
+_LOGGER = logging.getLogger(__name__)
 
-_LOGGER: logging.Logger = logging.getLogger(__package__)
+DOMAIN = "audiobookshelf"
+SCAN_INTERVAL = timedelta(seconds=10)
 
+async def count_active_users(data: dict) -> int:
+    """
+    Takes in an object with an array of users
+    and counts the active ones minus
+    the dummy hass one
+    """
+    count = 0
+    for user in data["users"]:
+        if user["isActive"] and user["username"] != "hass":
+            if ("token" in user and user["token"] == API_KEY):
+                continue  # Skip user with provided access_token
+            count += 1
+    return count
 
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
-    async_add_entities: AddEntitiesCallback,
-) -> None:
-    """Setup sensor platform."""
-    coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([
-        AudiobookshelfSessionsSensor(coordinator, entry),
-        AudiobookshelfNumberOfLibrariesSensor(coordinator, entry),
-    ])
+async def clean_user_attributes(data: dict):
+    """
+    Removes the token and some extra data from users
+    """
+    for user in data["users"]:
+        user["token"] = "<redacted>"
+    return data
 
+async def count_open_sessions(data: dict) -> int:
+    """
+    Counts the number of open stream sessions
+    """
+    return len(data["openSessions"])
 
-class AudiobookshelfSessionsSensor(AudiobookshelfEntity):
-    """audiobookshelf Sessions Sensor class."""
+async def count_libraries(data: dict) -> int:
+    """
+    Counts the number libraries
+    """
+    return len(data["libraries"])
 
-    def __init__(self, coordinator: CoordinatorEntity, entry: ConfigEntry) -> None:
-        self._attr_name = f"{DOMAIN} Sessions"
-        self._attr_device_class = f"{DOMAIN}__custom_device_class"
-        # self._attr_device_info = ...  # For automatic device registration
-        self._attr_unique_id = f"sensor.{DOMAIN}_sessions"
-        self._attr_icon ="mdi:format-quote-close"
-        super().__init__(coordinator, entry)
+async def extract_library_details(data: dict) -> dict:
+    details = {}
+    for library in data.get('libraries', []):
+        details.update({library['id']: {"mediaType": library['mediaType'],"provider": library['provider']}})
+    return details
+
+def get_total_duration(total_duration: float):
+    """Calculate the total duration in hours and round it to 0 decimal places."""
+    return round(total_duration / 60 / 60, 0)
+
+def get_total_size(total_size: float):
+    return round(total_size / 1024 / 1024 / 1024, 2)
+
+async def fetch_library_stats(session, id):
+    """Fetch data from a single endpoint."""
+    headers = {"Authorization": f"Bearer {API_KEY}"}
+    endpoint = f"api/libraries/{id}/stats"
+    try:
+        async with session.get(f"{API_URL}/{endpoint}", headers=headers) as response:
+            if response.status != 200:
+                _LOGGER.error(f"Failed to fetch data from {endpoint}, status: {response.status}")
+                return None
+            return await response.json()
+    except Exception as e:
+        _LOGGER.error(f"Exception occurred while fetching data from {endpoint}: {e}")
+        return None
+
+async def get_library_stats(data: dict) -> dict:
+    library_details = await extract_library_details(data)
+    async with aiohttp.ClientSession() as session:
+        results = {}
+        for id in library_details:
+            library_stats = await fetch_library_stats(session, id)
+            if isinstance(library_stats, Exception):
+                _LOGGER.error(f"Error fetching data: {library_stats}")
+            else:
+                # response for a decent library will be HUGE if we don't pick and choose bits
+                summary = {}
+                if library_details[id]["mediaType"] == "book":
+                    summary.update({"totalAuthors":library_stats["totalAuthors"]})
+                    if library_stats["totalAuthors"] is not None:
+                        summary.update({"totalAuthors":library_stats["totalAuthors"]})
+                    else: 
+                        summary.update({"totalAuthors": "0"})
+                elif library_details[id]["mediaType"] == "podcast":
+                    if library_stats["numAudioTracks"] is not None:
+                        summary.update({"numAudioTracks":library_stats["numAudioTracks"]})
+                    else: 
+                        summary.update({"numAudioTracks": "0"})
+
+                if library_stats["totalItems"] is not None:
+                    summary.update({"totalItems":library_stats["totalItems"]})
+                else: 
+                    summary.update({"totalItems": "0"})
+
+                if library_stats["totalSize"] is not None:
+                    summary.update({"totalSize": f"{get_total_size(library_stats["totalSize"])}GB"})
+                else: 
+                    summary.update({"totalSize": "0 GB"})
+
+                if library_stats["totalDuration"] is not None:
+                    summary.update({"totalDuration": f"{get_total_duration(library_stats["totalDuration"])} hours"})
+                else: 
+                    summary.update({"totalDuration": "0 hours"})
+
+                results.update({id: summary})
+        return results
+
+async def do_nothing(data):
+    return data
+
+type Sensor = dict[str, Any]
+
+# simple polling sensors
+sensors: dict[str, Sensor] = {
+    "users": {
+        "endpoint": "api/users", 
+        "name": "Audiobookshelf Users", 
+        "data_function": count_active_users,
+        "attributes_function": clean_user_attributes
+    },
+    "sessions": {
+        "endpoint": "api/users/online", 
+        "name": "Audiobookshelf Open Sessions", 
+        "data_function": count_open_sessions,
+        "attributes_function": do_nothing
+    },
+    "libraries": {
+        "endpoint": "api/libraries", 
+        "name": "Audiobookshelf Libraries", 
+        "data_function": count_libraries,
+        "attributes_function": get_library_stats
+    },
+}
+
+# async def fetch_libraries():
+#     headers = {"Authorization": f"Bearer {API_KEY}"}
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(f"{API_URL}/api/libraries", headers=headers) as response:
+#             return await response.json()
+
+# async def async_remove_sensors(hass: HomeAssistant, entity_type, sensor_type: str):
+#     """Remove all existing sensors of a specific type."""
+#     entity_registry: EntityRegistry = async_get_entity_registry(hass)
+
+#     entities_to_remove = []
+
+#     for entity in entity_registry.entities.values():
+#         if entity.domain == entity_type:
+#             state = hass.states.get(entity.entity_id)
+#             if state and state.attributes.get("sensor_type") == sensor_type:
+#                 entities_to_remove.append(entity.entity_id)
+
+# async def async_refresh_libraries(hass: HomeAssistant, async_add_entities):
+#     """Periodically refresh the sensors."""
+#     while True:
+#         _LOGGER.error("Refreshing Audiobookshelf library stats")
+#         libraries_data = await fetch_libraries()
+#         # remove and re-add library sensors
+#         await async_remove_sensors(hass, "sensor", "audiobookshelf_library")
+#         coordinator = AudiobookshelfDataUpdateCoordinator(hass)
+#         await coordinator.async_config_entry_first_refresh()
+
+#         ids = get_library_ids(libraries_data)
+#         entities = []
+#         for id in ids:
+#             entities.append(
+#                 AudiobookshelfSensor(
+#                     coordinator, {
+#                         "endpoint": f"api/libraries/{id}/stats", 
+#                         "name": f"Audiobookshelf Library {id}", 
+#                         "data_function": do_nothing,
+#                         "attributes_function": do_nothing,
+#                         "type": "audiobookshelf_library"
+#                     },
+#                 )
+#             )
+#         async_add_entities(entities, True)
+#         await asyncio.sleep(60)  # Wait for 1 minute before refreshing again
+
+async def async_setup_platform(hass: HomeAssistant, config, async_add_entities, discovery_info=None):
+    """Set up the sensor platform."""
+
+    coordinator = AudiobookshelfDataUpdateCoordinator(hass)
+    await coordinator.async_config_entry_first_refresh()
+
+    entities = [
+        AudiobookshelfSensor(coordinator, sensors["users"]),
+        AudiobookshelfSensor(coordinator, sensors["sessions"]),
+        AudiobookshelfSensor(coordinator, sensors["libraries"])
+    ]
+    async_add_entities(entities, True)
+
+class AudiobookshelfDataUpdateCoordinator(DataUpdateCoordinator):
+    """Class to manage fetching Audiobookshelf data from the API."""
+
+    def __init__(self, hass: HomeAssistant):
+        """Initialize."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            name="audiobookshelf",
+            update_interval=SCAN_INTERVAL,
+        )
+
+    async def _async_update_data(self):
+        """Fetch data from API endpoint."""
+        headers = {"Authorization": f"Bearer {API_KEY}"}
+        data = {}
+        try:
+            async with aiohttp.ClientSession() as session:
+                for sensor in sensors:
+                    async with session.get(f"{API_URL}/{sensors[sensor]["endpoint"]}", headers=headers) as response:
+                        if response.status != 200:
+                            raise UpdateFailed(f"Error fetching data: {response.status}")
+                        data[sensors[sensor]["endpoint"]] = await response.json()
+            return data
+        except aiohttp.ClientError as err:
+            raise UpdateFailed(f"Error fetching data: {err}")
+
+class AudiobookshelfSensor(Entity):
+    """Representation of a sensor."""
+
+    def __init__(self, coordinator: AudiobookshelfDataUpdateCoordinator, sensor: Sensor):
+        """Initialize the sensor."""
+        self._name = sensor["name"]
+        self._endpoint = sensor["endpoint"]
+        self.coordinator = coordinator
+        self._state = None
+        self._attributes = {}
+        self._process_data = sensor["data_function"]
+        self._process_attributes = sensor["attributes_function"]
 
     @property
-    def state(self) -> int | None:
-        """Return the state of the sensor."""
-        try:
-            coordinator_get = self.coordinator.data.get(
-                "sessions",
-                "",
-            )
-            _LOGGER.debug("""sensor coordinator got: %s""", coordinator_get)
-
-            if isinstance(coordinator_get, int):
-                return coordinator_get
-
-            return None
-
-        except AttributeError:
-            _LOGGER.debug(
-                "sensor: AttributeError caught while accessing coordinator data.",
-            )
-            return None
-
-class AudiobookshelfNumberOfLibrariesSensor(AudiobookshelfEntity):
-    """audiobookshelf Number of Libraries Sensor class."""
-
-    def __init__(self, coordinator: CoordinatorEntity, entry: ConfigEntry) -> None:
-        self._attr_name = f"{DOMAIN} Libraries"
-        self._attr_device_class = f"{DOMAIN}__custom_device_class"
-        # self._attr_device_info = ...  # For automatic device registration
-        self._attr_unique_id = f"sensor.{DOMAIN}_libraries"
-        self._attr_icon ="mdi:format-quote-close"
-        super().__init__(coordinator, entry)
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
 
     @property
-    def state(self) -> int | None:
+    def state(self):
         """Return the state of the sensor."""
-        try:
-            coordinator_get: dict | str = self.coordinator.data.get(
-                "libraries",
-                "",
-            )
-            _LOGGER.debug("""sensor coordinator got: %s""", coordinator_get)
+        return self._state
 
-            if not isinstance(coordinator_get, str):
-                # count and return int
-                return len(coordinator_get["libraries"])
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        return self._attributes
 
-            return None
+    @property
+    def device_info(self):
+        """Return device information about this entity."""
+        return {
+            "identifiers": {(DOMAIN, "audiobookshelf_id")},
+            "name": "Audiobookshelf",
+            "manufacturer": "My Company",
+            "model": "My Model",
+            "sw_version": "1.0",
+        }
 
-        except AttributeError:
-            _LOGGER.debug(
-                "sensor: AttributeError caught while accessing coordinator data.",
-            )
-            return None
+    async def async_update(self):
+        """Fetch new state data for the sensor."""
+        data = self.coordinator.data
+        if data:
+            endpoint_data = data.get(self._endpoint, {})
+            if isinstance(endpoint_data, dict):
+                self._attributes.update(await self._process_attributes(endpoint_data))
+                self._state = await self._process_data(data = endpoint_data)
+            else:
+                _LOGGER.error("Expected endpoint_data to be a dictionary, got %s", type(endpoint_data))
+                _LOGGER.debug(f"Data: {endpoint_data}")
+
+    async def async_added_to_hass(self):
+        """When entity is added to hass."""
+        self.async_on_remove(
+            self.coordinator.async_add_listener(self.async_write_ha_state)
+        )
+
